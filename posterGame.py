d4c25cd8f26f260aac894d41362f74ea894a1541
Alex <olsonap@uwm.edu>
2022-12-19 16:11:41 -0600

    Added Pygame deployment option

---
+++ posterGame.py
@@ -0,0 +1,92 @@
+# importing required library
+import pygame
+import requests
+import time
+import io
+from urllib.request import urlopen
+
+# activate the pygame library .
+pygame.init()
+
+
+# create the display surface object
+# of specific dimension..e(X, Y).
+
+
+
+class mainLoop:
+    window_width = 1080/3
+    window_height = 1960/3
+    movie_id = None
+    loading = False
+    def check_for_image(self):
+        rsp = requests.get(f"http://192.168.86.35:8000/get-current-movie/{self.movie_id}")
+        data = rsp.json()
+        if data.get('msg',None) and self.loading == False:
+            #self.i += 1
+            #print("Check_for_image: " + str(self.i))
+            msg = data['msg']
+            if msg != 'Movie has not changed':
+                print("Check_for_image said to update image")
+                self.update_image(msg)
+        return
+
+    def update_image(self, data):
+        print("SCHEDULER STOPPED")
+        self.loading = True
+        self.movie_id = data['movie_id']
+        self.poster_path = data['poster_path']
+        # create a surface object, image is drawn on it.
+
+        image_str = urlopen(self.poster_path).read()
+        # create a file object (stream)
+        image_file = io.BytesIO(image_str)
+        image = pygame.image.load(image_file)
+        # Using blit to copy content from one surface to other
+        poster = pygame.transform.scale(image, (self.window_width, self.window_height*(27/32)))
+        # Using blit to copy content from one surface to other
+        self.scrn.blit(poster,(0, self.window_height*(5/32)))
+        # paint screen one time
+        pygame.display.flip()
+        self.loading = False
+        return
+
+    def main_loop(self):
+        self.scrn = pygame.display.set_mode((self.window_width, self.window_height))
+
+        # set the pygame window name
+        pygame.display.set_caption('image')
+
+        # create a surface object, image is drawn on it.
+        twilight = pygame.image.load('static/theatername.jpg').convert()
+        poster = pygame.image.load('static/default.jpeg').convert()
+        twilight = pygame.transform.scale(twilight, (self.window_width - 10, (self.window_height-10)*(5/32)))
+        poster = pygame.transform.scale(poster, (self.window_width - 20, self.window_height*(27/32)-20))
+        # Using blit to copy content from one surface to other
+        pygame.draw.rect(self.scrn, (255,255,255), (5,5,self.window_width - 10, self.window_height - 10), border_radius=12)
+        self.scrn.blit(twilight,(5,5))
+        self.scrn.blit(poster,(10, (self.window_height*(5/32)) + 10))
+
+        # paint screen one time
+        pygame.display.flip()
+        status = True
+        oldEpoch = time.time()
+        while (status):
+            if time.time() - oldEpoch >= .5:
+                print("Checking for image")
+                oldEpoch = time.time()
+                #self.check_for_image()
+            # iterate over the list of Event objects
+            # that was returned by pygame.event.get() method.
+            for i in pygame.event.get():
+
+                # if event object type is QUIT
+                # then quitting the pygame
+                # and program both.
+                if i.type == pygame.QUIT:
+                    status = False
+
+if __name__=="__main__":
+    mainLoop().main_loop()
+
+    pygame.quit()
